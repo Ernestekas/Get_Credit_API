8c1a5d0cf853126553b08cffdeb7d50802d91cae
Ernestekas <ernestekas@gmail.com>
2022-02-24 21:18:52 +0200

    Init

---
+++ GetCredit/GetCredit/Controllers/CreditController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetCredit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetRates(decimal creditValue)
+        {
+            return Ok();
+        }
+    }
+}
